4e4de3ba0de0cb8692ebe770cb9b2b6879069824
Ben Terova <ben@benterova.com>
2025-05-06 14:27:09 -0400

    Fix missing file submissions, updating styles

--- src/app/apply-modal/apply-modal.component.ts
+++ src/app/apply-modal/apply-modal.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
 import {
   NovoFormGroup,
   FormUtils,
@@ -11,20 +11,19 @@ import {
   NovoToastService,
   CheckboxControl,
   FieldInteractionApi,
-} from 'novo-elements';
-import { SettingsService } from '../services/settings/settings.service';
-import { AnalyticsService } from '../services/analytics/analytics.service';
-import { ApplyService } from '../services/apply/apply.service';
-import { Router } from '@angular/router';
-import { TranslateService } from '@ngx-translate/core';
+} from "novo-elements";
+import { SettingsService } from "../services/settings/settings.service";
+import { AnalyticsService } from "../services/analytics/analytics.service";
+import { ApplyService } from "../services/apply/apply.service";
+import { Router } from "@angular/router";
+import { TranslateService } from "@ngx-translate/core";
 
 @Component({
-  selector: 'app-apply-modal',
-  templateUrl: './apply-modal.component.html',
-  styleUrls: ['./apply-modal.component.scss'],
+  selector: "app-apply-modal",
+  templateUrl: "./apply-modal.component.html",
+  styleUrls: ["./apply-modal.component.scss"],
 })
 export class ApplyModalComponent implements OnInit {
-
   public job: any;
   public source: string;
   public firstName: TextBoxControl;
@@ -42,125 +41,157 @@ export class ApplyModalComponent implements OnInit {
   public eeocControls: any = [];
   public consentControl: any;
   public applying: boolean = false;
-  public consentCheckbox: boolean = SettingsService.settings.privacyConsent.consentCheckbox;
-  public showCategory: boolean  = SettingsService.settings.service.showCategory;
+  public consentCheckbox: boolean =
+    SettingsService.settings.privacyConsent.consentCheckbox;
+  public showCategory: boolean = SettingsService.settings.service.showCategory;
   public isIos: boolean = SettingsService.isIos;
-  private APPLIED_JOBS_KEY: string = 'APPLIED_JOBS_KEY';
+  private APPLIED_JOBS_KEY: string = "APPLIED_JOBS_KEY";
 
-  constructor(private formUtils: FormUtils,
-              public params: NovoModalParams,
-              private modalRef: NovoModalRef,
-              private applyService: ApplyService,
-              private analytics: AnalyticsService,
-              private toaster: NovoToastService,
-              private router: Router,
-              private translate: TranslateService,
-               ) { this.toaster.parentViewContainer = this.params['viewContainer']; }
+  constructor(
+    private formUtils: FormUtils,
+    public params: NovoModalParams,
+    private modalRef: NovoModalRef,
+    private applyService: ApplyService,
+    private analytics: AnalyticsService,
+    private toaster: NovoToastService,
+    private router: Router,
+    private translate: TranslateService,
+  ) {
+    this.toaster.parentViewContainer = this.params["viewContainer"];
+  }
 
   public ngOnInit(): void {
-    this.job = this.params['job'];
-    this.source = this.params['source'];
+    this.job = this.params["job"];
+    this.source = this.params["source"];
     this.setupForm();
   }
   public setupForm(): void {
     this.firstName = new TextBoxControl({
-      key: 'firstName',
-      label: this.translate.instant('FIRST_NAME'),
+      key: "firstName",
+      label: this.translate.instant("FIRST_NAME"),
       required: true,
       hidden: false,
-      value: '',
+      value: "",
     });
     this.lastName = new TextBoxControl({
-      key: 'lastName',
-      label: this.translate.instant('LAST_NAME'),
+      key: "lastName",
+      label: this.translate.instant("LAST_NAME"),
       required: true,
       hidden: false,
-      value: '',
+      value: "",
     });
     this.email = new TextBoxControl({
-      key: 'email',
-      label: this.translate.instant('EMAIL'),
-      type: 'email',
+      key: "email",
+      label: this.translate.instant("EMAIL"),
+      type: "email",
       required: true,
       hidden: false,
-      value: '',
+      value: "",
     });
     this.phoneNumber = new TextBoxControl({
-      key: 'phone',
-      label: this.translate.instant('PHONE'),
-      type: 'tel',
+      key: "phone",
+      label: this.translate.instant("PHONE"),
+      type: "tel",
       required: false,
       hidden: false,
-      value: '',
+      value: "",
     });
     this.genderRaceEthnicity = [
       new SelectControl({
-        key: 'gender',
-        label: this.translate.instant('EEOC.GENDER_LABEL'),
+        key: "gender",
+        label: this.translate.instant("EEOC.GENDER_LABEL"),
         required: SettingsService.settings.eeoc.genderRaceEthnicity,
         hidden: false,
         options: [
-          { value: 'M', label: this.translate.instant('EEOC.GENDER_MALE') },
-          { value: 'F', label: this.translate.instant('EEOC.GENDER_FEMALE')},
-          { value: 'D', label: this.translate.instant('EEOC.GENDER_ND')},
+          { value: "M", label: this.translate.instant("EEOC.GENDER_MALE") },
+          { value: "F", label: this.translate.instant("EEOC.GENDER_FEMALE") },
+          { value: "D", label: this.translate.instant("EEOC.GENDER_ND") },
         ],
-      }), new PickerControl({
-        key: 'ethnicity',
-        label: this.translate.instant('EEOC.RACE_ETHNICITY_LABEL'),
+      }),
+      new PickerControl({
+        key: "ethnicity",
+        label: this.translate.instant("EEOC.RACE_ETHNICITY_LABEL"),
         required: SettingsService.settings.eeoc.genderRaceEthnicity,
         hidden: false,
         multiple: true,
-        placeholder: this.translate.instant('EEOC.SELECT_ALL'),
+        placeholder: this.translate.instant("EEOC.SELECT_ALL"),
         config: {
           options: [
-            { value: 'HL', label: this.translate.instant('EEOC.RACE_ETHNICITY_HL') },
-            { value: 'WH', label: this.translate.instant('EEOC.RACE_ETHNICITY_WH') },
-            { value: 'BL', label: this.translate.instant('EEOC.RACE_ETHNICITY_BL') },
-            { value: 'AS', label: this.translate.instant('EEOC.RACE_ETHNICITY_AS') },
-            { value: 'NP', label: this.translate.instant('EEOC.RACE_ETHNICITY_NP') },
-            { value: 'IA', label: this.translate.instant('EEOC.RACE_ETHNICITY_IA') },
-            { value: 'DN', label: this.translate.instant('EEOC.RACE_ETHNICITY_DN') },
+            {
+              value: "HL",
+              label: this.translate.instant("EEOC.RACE_ETHNICITY_HL"),
+            },
+            {
+              value: "WH",
+              label: this.translate.instant("EEOC.RACE_ETHNICITY_WH"),
+            },
+            {
+              value: "BL",
+              label: this.translate.instant("EEOC.RACE_ETHNICITY_BL"),
+            },
+            {
+              value: "AS",
+              label: this.translate.instant("EEOC.RACE_ETHNICITY_AS"),
+            },
+            {
+              value: "NP",
+              label: this.translate.instant("EEOC.RACE_ETHNICITY_NP"),
+            },
+            {
+              value: "IA",
+              label: this.translate.instant("EEOC.RACE_ETHNICITY_IA"),
+            },
+            {
+              value: "DN",
+              label: this.translate.instant("EEOC.RACE_ETHNICITY_DN"),
+            },
           ],
         },
       }),
     ];
     this.veteran = [
       new SelectControl({
-        key: 'veteran',
-        label: this.translate.instant('EEOC.VETERAN_LABEL'),
-        description: this.translate.instant('EEOC.VETERAN_DESCRIPTION'),
+        key: "veteran",
+        label: this.translate.instant("EEOC.VETERAN_LABEL"),
+        description: this.translate.instant("EEOC.VETERAN_DESCRIPTION"),
         required: SettingsService.settings.eeoc.veteran,
         hidden: false,
         options: [
-          { value: 'P', label: this.translate.instant('EEOC.VETERAN_P') },
-          { value: 'V', label: this.translate.instant('EEOC.VETERAN_V')},
-          { value: 'N', label: this.translate.instant('EEOC.VETERAN_N')},
-          { value: 'D', label: this.translate.instant('EEOC.VETERAN_D')},
+          { value: "P", label: this.translate.instant("EEOC.VETERAN_P") },
+          { value: "V", label: this.translate.instant("EEOC.VETERAN_V") },
+          { value: "N", label: this.translate.instant("EEOC.VETERAN_N") },
+          { value: "D", label: this.translate.instant("EEOC.VETERAN_D") },
         ],
       }),
     ];
     this.disability = [
       new SelectControl({
-        key: 'disability',
-        label: this.translate.instant('EEOC.DISABILITY_LABEL'),
-        description: this.translate.instant('EEOC.DISABILITY_DESCRIPTION'),
+        key: "disability",
+        label: this.translate.instant("EEOC.DISABILITY_LABEL"),
+        description: this.translate.instant("EEOC.DISABILITY_DESCRIPTION"),
         required: SettingsService.settings.eeoc.disability,
         hidden: false,
         options: [
-          { value: 'Y', label: this.translate.instant('EEOC.DISABILITY_Y') },
-          { value: 'N', label: this.translate.instant('EEOC.DISABILITY_N')},
-          { value: 'D', label: this.translate.instant('EEOC.DISABILITY_D')},
+          { value: "Y", label: this.translate.instant("EEOC.DISABILITY_Y") },
+          { value: "N", label: this.translate.instant("EEOC.DISABILITY_N") },
+          { value: "D", label: this.translate.instant("EEOC.DISABILITY_D") },
         ],
       }),
     ];
     this.resume = new FileControl({
-      key: 'resume',
+      key: "resume",
       required: false,
       hidden: false,
-      description: `${this.translate.instant('ACCEPTED_RESUME')} ${SettingsService.settings.acceptedResumeTypes.toString()}`,
+      description: `${this.translate.instant("ACCEPTED_RESUME")} ${SettingsService.settings.acceptedResumeTypes.toString()}`,
     });
 
-    this.formControls = [this.firstName, this.lastName, this.email, this.phoneNumber, this.resume];
+    this.formControls = [
+      this.firstName,
+      this.lastName,
+      this.email,
+      this.phoneNumber,
+      this.resume,
+    ];
 
     let eeoc: any = SettingsService.settings.eeoc;
     for (let field in eeoc) {
@@ -170,22 +201,26 @@ export class ApplyModalComponent implements OnInit {
     }
 
     this.consentControl = new CheckboxControl({
-      key: 'consent',
+      key: "consent",
       required: SettingsService.settings.privacyConsent.consentCheckbox,
       hidden: false,
       interactions: [
         {
-          event: 'change',
+          event: "change",
           script: (FAPI: FieldInteractionApi) => {
-            if (!FAPI.getValue('consent')) {
-              FAPI.markAsInvalid('consent');
+            if (!FAPI.getValue("consent")) {
+              FAPI.markAsInvalid("consent");
             }
           },
         },
       ],
     });
 
-    this.form = this.formUtils.toFormGroup([...this.formControls, ...this.eeocControls, this.consentControl]);
+    this.form = this.formUtils.toFormGroup([
+      ...this.formControls,
+      ...this.eeocControls,
+      this.consentControl,
+    ]);
     this.loading = false;
   }
 
@@ -193,7 +228,9 @@ export class ApplyModalComponent implements OnInit {
     if (applied) {
       this.analytics.trackEvent(`Success applying to job ${this.job.id}`);
     } else {
-      this.analytics.trackEvent(`Close apply form without applying for job ${this.job.id}`);
+      this.analytics.trackEvent(
+        `Close apply form without applying for job ${this.job.id}`,
+      );
     }
     this.modalRef.close(undefined);
   }
@@ -202,12 +239,17 @@ export class ApplyModalComponent implements OnInit {
     if (this.form.valid) {
       this.applying = true;
       this.analytics.trackEvent(`Apply to Job: ${this.job.id}`);
+      const format = this.form.value.resume[0]
+        ? this.form.value.resume[0].name.substring(
+            this.form.value.resume[0].name.lastIndexOf(".") + 1,
+          )
+        : "text";
       let requestParams: any = {
         firstName: encodeURIComponent(this.form.value.firstName),
         lastName: encodeURIComponent(this.form.value.lastName),
         email: encodeURIComponent(this.form.value.email),
-        phone: encodeURIComponent(this.form.value.phone || ''),
-        format: this.form.value.resume[0].name.substring(this.form.value.resume[0].name.lastIndexOf('.') + 1),
+        phone: encodeURIComponent(this.form.value.phone || ""),
+        format: format,
       };
 
       if (this.form.value.gender) {
@@ -220,33 +262,44 @@ export class ApplyModalComponent implements OnInit {
         requestParams.veteran = encodeURIComponent(this.form.value.veteran);
       }
       if (this.form.value.disability) {
-        requestParams.disability = encodeURIComponent(this.form.value.disability);
+        requestParams.disability = encodeURIComponent(
+          this.form.value.disability,
+        );
       }
       if (this.source) {
         requestParams.source = this.source;
       }
 
       let formData: FormData = new FormData();
-      formData.append('resume', this.form.value.resume[0].file);
-      this.applyService.apply(this.job.id, requestParams, formData).subscribe(this.applyOnSuccess.bind(this), this.applyOnFailure.bind(this) );
+      if (this.form.value.resume[0]) {
+        formData.append("resume", this.form.value.resume[0].file);
+      }
+      this.applyService
+        .apply(this.job.id, requestParams, formData)
+        .subscribe(
+          this.applyOnSuccess.bind(this),
+          this.applyOnFailure.bind(this),
+        );
     }
   }
 
   public viewPrivacyPolicy(): void {
-    const url: string = SettingsService.settings.privacyConsent.privacyPolicyUrl;
-    if (url === '/privacy') {
+    const url: string =
+      SettingsService.settings.privacyConsent.privacyPolicyUrl;
+    if (url === "/privacy") {
       this.router.navigate([url]);
     } else {
       window.open(url);
-    }  }
+    }
+  }
 
   private applyOnSuccess(res: any): void {
     let toastOptions: any = {
-      theme: 'success',
-      icon: 'check',
-      title: this.translate.instant('THANK_YOU'),
-      message: this.translate.instant('YOU_WILL_BE_CONTACTED'),
-      position: 'growlTopRight',
+      theme: "success",
+      icon: "check",
+      title: this.translate.instant("THANK_YOU"),
+      message: this.translate.instant("YOU_WILL_BE_CONTACTED"),
+      position: "growlTopRight",
       hideDelay: 3000,
     };
     this.toaster.alert(toastOptions);
@@ -254,9 +307,15 @@ export class ApplyModalComponent implements OnInit {
     if (alreadyAppliedJobs) {
       let alreadyAppliedJobsArray: any = JSON.parse(alreadyAppliedJobs);
       alreadyAppliedJobsArray.push(this.job.id);
-      sessionStorage.setItem(this.APPLIED_JOBS_KEY, JSON.stringify(alreadyAppliedJobsArray));
+      sessionStorage.setItem(
+        this.APPLIED_JOBS_KEY,
+        JSON.stringify(alreadyAppliedJobsArray),
+      );
     } else {
-      sessionStorage.setItem(this.APPLIED_JOBS_KEY, JSON.stringify([this.job.id]));
+      sessionStorage.setItem(
+        this.APPLIED_JOBS_KEY,
+        JSON.stringify([this.job.id]),
+      );
     }
     this.applying = false;
     this.close(true);
--- src/app/services/apply/apply.service.ts
+++ src/app/services/apply/apply.service.ts
@@ -1,25 +1,26 @@
-import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
-import { SettingsService } from '../settings/settings.service';
-import { Observable } from 'rxjs';
-import { IServiceSettings } from '../../typings/settings';
+import { Injectable } from "@angular/core";
+import { HttpClient } from "@angular/common/http";
+import { SettingsService } from "../settings/settings.service";
+import { Observable } from "rxjs";
+import { IServiceSettings } from "../../typings/settings";
 
 @Injectable()
 export class ApplyService {
-
-  public constructor(private http: HttpClient, public settings: SettingsService) {
-
-  }
+  public constructor(
+    private http: HttpClient,
+    public settings: SettingsService,
+  ) {}
   get baseUrl(): string {
     let service: IServiceSettings = SettingsService.settings.service;
     let port: number = service.port ? service.port : 443;
-    let scheme: string = `http${ port === 443  ? 's' : '' }`;
+    let scheme: string = `http${port === 443 ? "s" : ""}`;
 
     return `${scheme}://public-rest${service.swimlane}.bullhornstaffing.com:${port}/rest-services/${service.corpToken}/apply`;
   }
 
   public apply(id: number, params: any, formData: FormData): Observable<any> {
     let applyParams: any = this.assembleParams(params);
+    formData = this.createMockFile(formData);
     return this.http.post(`${this.baseUrl}/${id}/raw?${applyParams}`, formData);
   }
 
@@ -28,11 +29,24 @@ export class ApplyService {
     params.push(`externalID=Resume`);
     params.push(`type=Resume`);
     for (let key in data) {
-      if (!data.hasOwnProperty(key)) { continue; }
-      if (!data[key]) { continue; }
+      if (!data.hasOwnProperty(key)) {
+        continue;
+      }
+      if (!data[key]) {
+        continue;
+      }
       let value: any = data[key];
       params.push(`${key}=${value}`);
     }
-    return params.join('&');
+    return params.join("&");
+  }
+
+  private createMockFile(data: FormData) {
+    let resume = data.get("resume");
+    if (!resume) {
+      const file = new Blob([data.get("email")], { type: "text/plain" });
+      data.set("resume", file);
+    }
+    return data;
   }
 }
